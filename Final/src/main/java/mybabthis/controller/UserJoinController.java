8d3bba7a3dc49a7b8a38bd47b178266e1c195fb0
Hyunse <Hyunse@59.6.86.191>
2015-02-07 19:36:01 +0900

    모두들 화이팅입니다

--- Final/src/main/java/mybabthis/controller/UserJoinController.java
+++ Final/src/main/java/mybabthis/controller/UserJoinController.java
@@ -11,7 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserJoinController {
@@ -27,8 +28,8 @@ public class UserJoinController {
 	
 	@RequestMapping(value="/join", method=RequestMethod.GET)
 	public String enterLogin(Model model){
-		
-		return "/join";
+		model.addAttribute("Users", new Users());
+		return "join";
 		
 	}
 
@@ -41,13 +42,28 @@ public class UserJoinController {
 		return "redirect:/main/main";
 		
 	}
-
+	@RequestMapping(value="/id", method=RequestMethod.GET)
+	public String ajaxPageCall(Model model){
 	
-	@RequestMapping(value="/main/main", method=RequestMethod.GET)
-	public String goMain(){
-		
-		
-		return "/main/main";
+		model.addAttribute("Users", new Users());
+		return "join";
 		
 	}
+
+
+	@RequestMapping(value="/id", method=RequestMethod.POST, produces="text/plain;charset=utf-8")
+	public @ResponseBody String ajaxReceive(@RequestParam String id){
+		
+		logger.trace("-------------------------------------------------------------------------------");
+		
+		Users user =userservice.selectUser(id);
+		
+		if(user != null){
+			logger.trace("----------------------중복 아이디");
+			return "중복된 아이디 입니다.";
+		}
+		logger.trace("----------------------사용가능 아이디");
+		return "사용 가능한 아이디 입니다.";
+	}
+	
 }
--- Final/src/main/java/mybabthis/controller/UserLoginController.java
+++ Final/src/main/java/mybabthis/controller/UserLoginController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-
 @Controller
 @SessionAttributes("loginUser")
 public class UserLoginController {
