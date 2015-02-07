77dd4b46166db7ccd7d3aa5a96b52125150884c5
r <r@r-PC>
2015-02-07 15:44:39 +0900

    자유게시판 BoardDao/BoardDaoImpl/BoardMapper.xml 생성함 .

--- Final/src/main/java/mybabthis/test/ContextTest.java
+++ Final/src/main/java/mybabthis/test/ContextTest.java
@@ -2,10 +2,14 @@ package mybabthis.test;
 
 import javax.sql.DataSource;
 
+import mybabthis.dao.BoardDao;
+import mybabthis.entity.Board;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 
 
@@ -16,11 +20,22 @@ public class ContextTest {
 	}
 	
 	
+	
 	public static void main(String[] args) {
 		
 		ApplicationContext ctx = new GenericXmlApplicationContext("spring/application_config.xml");
 		DataSource source = ctx.getBean(DataSource.class);
 		logger.trace("datasource 확인 : " + source);
+		JdbcTemplate temp = ctx.getBean(JdbcTemplate.class);
+		logger.trace("temp : " + temp);
+		
+		BoardDao dao = ctx.getBean(BoardDao.class);
+		Board board = new Board();
+		board.setBoardContent("정발산 ㄱㄱㄱㄱ");
+		board.setBoardTitle("정발산ㄱㄱㄱㄱㄱ");
+		board.setBoardType("F");
+		
+		logger.trace("insert : " + dao.createBoard(board));
 		
 	}
 	
