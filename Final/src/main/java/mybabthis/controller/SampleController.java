0c55b667843552db3a41656ca20c6222f8a2aa6c
seungyeon <seungyeon@Seungyeon>
2015-02-12 15:43:37 +0900

    SampleGrid

--- Final/src/main/java/mybabthis/controller/SampleController.java
+++ Final/src/main/java/mybabthis/controller/SampleController.java
@@ -5,16 +5,14 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import mybabthis.entity.Users;
-import mybabthis.service.UserService;
+import mybabthis.entity.Restaurant;
+import mybabthis.service.RestaurantService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.google.gson.Gson;
@@ -27,7 +25,7 @@ public class SampleController {
 	}
 
 	@Autowired
-	UserService userService;
+	RestaurantService restaurantService;
 
 	/*@RequestMapping(value = "/users", method = RequestMethod.GET)
 	public String userListAll(Model model) {
@@ -56,16 +54,11 @@ public class SampleController {
 	@RequestMapping(value = "/a")
 	public ModelAndView list(HttpServletRequest request, HttpSession session) throws Exception {
 		ModelAndView mav = new ModelAndView();
-		List<Users> list = userService.selectAllUser();
-		
-		String abc = "{\"total\":1,\"records\":[{\"userId\":\"aaa\"}]}";
-		String json = new Gson().toJson(list);
-		String jsonString = "{\"total\":" + list.size() + ", \"records\" :"
-				+ json + "}";
-		
+		List<Restaurant> restaurants = restaurantService.selectAllRestuarants();
+		String json = new Gson().toJson(restaurants);
+		String jsonString = "{\"total\":" + restaurants.size() + ", \"records\" :"+ json + "}";
 		mav.addObject("jsonString", jsonString);
 		logger.trace(jsonString);
-		logger.trace(abc);
 		mav.setViewName("../../listData");
 		return mav;
 	}
